72003f1734ac163bcf4c52da9248139aae341bcb
Chris Lemke <spcbrn1@gmail.com>
2017-11-14 10:08:16 -0700

    demo ready

--- src/components/ChartDemos/LiveDemo/LiveDemo.js
+++ src/components/ChartDemos/LiveDemo/LiveDemo.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import {Doughnut} from 'react-chartjs-2';
 
 
 
 const LiveDemo = () => {
+  const data = {
+    labels: ['Nick', 'Ana', 'Beth', 'Eunbin', 'Fernando'],
+    datasets: [
+      {
+        data: [23, 23, 22, 24, 24],
+        backgroundColor: [
+          '#424242',
+          '#353535',
+          '#000',
+          'tomato',
+          'dodgerblue'
+        ],
+        hoverBackgroundColor: '#FEFEFE'
+    },
+    {
+      data: [23, 23, 22, 24, 24],
+      type: 'line',
+      backgroundColor: [
+        '#424242',
+        '#353535',
+        '#000',
+        'tomato',
+        'dodgerblue'
+      ],
+      hoverBackgroundColor: '#FEFEFE'
+  }
+    ]
+  }
 
   return (
     <main className="demo_wrapper">
@@ -11,7 +40,7 @@ const LiveDemo = () => {
       <p>I need a chart!!</p>
 
     </header>
-
+    <Doughnut data={data} />
     </main>
   )
 }
